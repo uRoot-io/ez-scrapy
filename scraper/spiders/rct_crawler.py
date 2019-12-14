86c3db7cdb4c3ec09b5e5576783413ff392b3f0d
uRoot-io <equipe@uroot.io>
2019-12-14 01:40:54 +0000

    Fixed crawler!

---
+++ scraper/spiders/rct_crawler.py
@@ -0,0 +1,26 @@
+import scrapy
+from scraper.items import ScraperItem
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+
+class MySpider(CrawlSpider):
+    name = 'rct-crawler'
+    allowed_domains = ['toutsurlisolation.com']
+    start_urls = ['https://toutsurlisolation.com']
+    base_url = 'https://toutsurlisolation.com/'
+
+    rules = (Rule(LinkExtractor(allow=(r'^https?://toutsurlisolation.com/.*', ))), )
+
+    def parse(self, response):
+
+        item = ScraperItem()
+        item['url'] = response.url
+        item['title'] = response.xpath('//title/text()').get()
+        item['meta'] = response.xpath("//meta[@name='description']/@content").extract()
+
+        yield item
+
+        for url in response.selector.xpath('//a/@href').extract():
+            yield scrapy.http.Request(response.urljoin(url), callback=self.parse)
+
+SPIDER = MySpider()
